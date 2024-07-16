ffda8a8e9998c27c656ac73001cd3a633f107ca5
John-Embate <72902619+John-Embate@users.noreply.github.com>
2024-07-16 19:14:23 +0800

    update chatbot credential config

--- chatbot_admin.py
+++ chatbot_admin.py
@@ -27,13 +27,25 @@ if "connext_chatbot_admin_credentials" not in st.session_state:
 
 #Configure this one to True if deployed on streamlit community cloud or on local machine
 #This helps change the json file and api key loading
-is_streamlit_deployed = True
+is_streamlit_deployed = False
+
+def get_credentials_dict(credentials_input):
+    if isinstance(credentials_input, str):
+        try:
+            # Attempt to parse the string as JSON
+            return json.loads(credentials_input)
+        except json.JSONDecodeError:
+            raise ValueError("Credentials string is not valid JSON")
+    elif isinstance(credentials_input, dict):
+        return credentials_input
+    else:
+        raise ValueError("Credentials must be a JSON string or a dictionary")
 
 if is_streamlit_deployed:
     # Load the JSON content from Streamlit secrets
     service_account_info = st.secrets["gcp_service_account"]
     # Convert the TOML object to a dictionary
-    st.session_state["connext_chatbot_admin_credentials"] = service_account_info 
+    st.session_state["connext_chatbot_admin_credentials"] = get_credentials_dict(service_account_info) 
     st.session_state["api_keys"]["FIREBASE_API_KEY"] = st.secrets["FIREBASE_API_KEY"]
     st.session_state["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"] = st.secrets["GOOGLE_AI_STUDIO_API_KEY"]
 else:
@@ -44,6 +56,7 @@ else:
     google_ai_api_key = os.getenv('GOOGLE_AI_STUDIO_API_KEY')
     st.session_state["api_keys"]["FIREBASE_API_KEY"] = firebase_api_key
     st.session_state["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"] = google_ai_api_key 
+
 #Firebase SDK initialization
 if not firebase_admin._apps:
     cred = credentials.Certificate(st.session_state["connext_chatbot_admin_credentials"])
