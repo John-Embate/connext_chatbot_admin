ceed8c5846525726efb521f7a481220cb35dbc7b
John-Embate <72902619+John-Embate@users.noreply.github.com>
2024-07-27 18:33:17 +0800

    configure googel oauth options.

--- chatbot_admin.py
+++ chatbot_admin.py
@@ -14,6 +14,10 @@ import tempfile
 from functools import partial
 import datetime
 import mimetypes
+#Google Auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 #external python files
 import configuration, playground
 
@@ -31,15 +35,27 @@ is_streamlit_deployed = True
 firebase_api_key = None
 google_ai_api_key = None
 
+def get_credentials_dict(credentials_input):
+    if isinstance(credentials_input, str):
+        try:
+            # Attempt to parse the string as JSON
+            return json.loads(credentials_input)
+        except json.JSONDecodeError:
+            raise ValueError("Credentials string is not valid JSON")
+    elif isinstance(credentials_input, dict):
+        return credentials_input
+    else:
+        raise ValueError("Credentials must be a JSON string or a dictionary")
+
 if is_streamlit_deployed:
     # Load the JSON content from Streamlit secrets
     service_account_info = st.secrets["gcp_service_account"]
-    st.session_state["connext_chatbot_admin_credentials"] = dict(service_account_info)
+    # Convert the TOML object to a dictionary
+    st.session_state["connext_chatbot_admin_credentials"] = service_account_info 
     st.session_state["api_keys"]["FIREBASE_API_KEY"] = st.secrets["FIREBASE_API_KEY"]
     st.session_state["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"] = st.secrets["GOOGLE_AI_STUDIO_API_KEY"]
     firebase_api_key = st.secrets["FIREBASE_API_KEY"]
     google_ai_api_key = st.secrets["GOOGLE_AI_STUDIO_API_KEY"]
-    
 else:
     with open('connext-chatbot-admin-ce0eb842ce8e.json') as f:
         st.session_state["connext_chatbot_admin_credentials"] = json.load(f)
@@ -55,8 +71,51 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 
-### For Log-in Page: Start ###
+SCOPES = ['https://www.googleapis.com/auth/generative-language.retriever']
 
+def load_creds():
+    """Converts `client_secret.json` to a credential object.
+
+    This function caches the generated tokens to minimize the use of the
+    consent screen.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = None
+            if not is_streamlit_deployed:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'connext_chatbot_auth.json', SCOPES)
+            else:
+                # Load client config from Streamlit secrets
+                client_config = {
+                    "installed": {
+                        "client_id": st.secrets["google_oauth"]["client_id"],
+                        "project_id": st.secrets["google_oauth"]["project_id"],
+                        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                        "token_uri": "https://oauth2.googleapis.com/token",
+                        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                        "client_secret": st.secrets["google_oauth"]["client_secret"],
+                        "redirect_uris": ["http://localhost"]
+                    }
+                }
+                # Initiate the flow using the client configuration from secrets
+                flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+### For Log-in Page: Start ###
 
 ## Functions: Start ###
 @st.experimental_dialog("Logging In Failed")
@@ -66,7 +125,7 @@ def fail_login_dialog(message):
 
 def sign_in_with_email_and_password(email=None, password=None, return_secure_token=True):
         rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
-    
+        
         try:
             payload = {
                 "returnSecureToken": return_secure_token
