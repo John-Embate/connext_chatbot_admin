a2aac57cbaaf71b401dc8e66e7e9545730b2e04c
John-Embate <72902619+John-Embate@users.noreply.github.com>
2024-07-27 20:47:08 +0800

    Update playground.py

--- playground.py
+++ playground.py
@@ -32,7 +32,7 @@ SCOPES = ['https://www.googleapis.com/auth/generative-language.retriever']
 
 @st.dialog("Google Consent Authentication Link")
 def google_oauth_link(flow):
-    auth_url, _ = flow.authorization_url(prompt='consent')
+    auth_url, _ = flow.authorization_url(redirect_uri="https://connext-chatbot-config.streamlit.app/", prompt='consent')
     st.write("Please go to this URL and authorize access:")
     st.markdown(f"[Sign in with Google]({auth_url})", unsafe_allow_html=True)
     code = st.text_input("Enter the authorization code:")
